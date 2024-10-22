f8e584bfd1d7ede9f7d3e65766f5af3d17e713d2
Trenton Elliott <trentonelliott1424@gmail.com>
2024-10-22 09:15:59 -0500

    REMOVED EXTRA COMMAND

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -1,7 +1,6 @@
 echo "Logging into Sandbox Salesforce Org"
 mkdir keys
 echo $SANDBOX_CERT_KEY | base64 -d > keys/server.key
-cat keys/server.key
 
 echo "Authenticating org"
 sf org login jwt --client-id $SANDBOX_APP_KEY --jwt-key-file keys/server.key --username $SANDBOX_USERNAME --set-default-dev-hub -a DevHub
