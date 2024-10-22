0675e57d87468048415909fb7513db435a907b83
Trenton Elliott <trentonelliott1424@gmail.com>
2024-10-22 08:50:57 -0500

    updated script to use sf command instead of sfdx

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -4,4 +4,4 @@ echo $SANDBOX_CERT_KEY | base64 -d > keys/server.key
 cat keys/server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub --loglevel DEBUG
+sf org login jwt --client-id $SANDBOX_APP_KEY --jwt-key-file keys/server.key --username $SANDBOX_USERNAME --set-default-dev-hub -a DevHub
