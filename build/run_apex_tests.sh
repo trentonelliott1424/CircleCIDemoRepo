5f288adf30957336e539b4f9aeb11526f4c709ff
Trenton Elliott <trentonelliott1424@gmail.com>
2024-10-23 10:55:46 -0500

    Uncommented code from script

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -3,16 +3,16 @@
 #sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
 
 #Create a scratch org
-#echo "Creating the Scratch Org..."
-#sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
+echo "Creating the Scratch Org..."
+sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
 
-#sfdx force:source:push -u ${CIRCLE_BRANCH}
+sfdx force:source:push -u ${CIRCLE_BRANCH}
 
-#echo "Coverting source to metadata format"
-#sfdx force:source:convert -d test_code -r force-app
+echo "Coverting source to metadata format"
+sfdx force:source:convert -d test_code -r force-app
 
-#echo "Deploying code to org"
-#sfdx force:mdapi:deploy --checkonly -u DevHub -d test_code/ -w -1 -l RunLocalTests
+echo "Deploying code to org"
+sfdx force:mdapi:deploy --checkonly -u DevHub -d test_code/ -w -1 -l RunLocalTests
 
 echo "Deploying source to org"
 sfdx force:source:deploy --sourcepath force-app --targetusername DevHub
