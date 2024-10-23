6acaf573a286974cfb0f97d8cad79008f6aefb68
Trenton Elliott <trentonelliott1424@gmail.com>
2024-10-23 10:38:50 -0500

    Updated script to handle key decryption

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -1,8 +1,6 @@
 echo "Logging into Sandbox Salesforce Org"
 mkdir keys
-echo "SANDBOX_CERT_KEY = '$SANDBOX_CERT_KEY'"
-echo -n $SANDBOX_CERT_KEY | base64 -d > keys/server.key
-
+echo -n $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 
 echo "Authenticating org"
 sf org login jwt --client-id $SANDBOX_APP_KEY --jwt-key-file keys/server.key --username $SANDBOX_USERNAME --set-default-dev-hub -a DevHub
