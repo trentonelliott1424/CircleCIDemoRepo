faa86d130b3441ebc220869eadc54aa8522d0417
Trenton Elliott <trentonelliott1424@gmail.com>
2024-10-23 11:00:22 -0500

    changed sourcepath to only deploy classes

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -3,19 +3,19 @@
 #sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
 
 #Create a scratch org
-echo "Creating the Scratch Org..."
-sfdx force:org:create -f config/project-scratch-def.json -a "testScratch" -s
+#echo "Creating the Scratch Org..."
+#sfdx force:org:create -f config/project-scratch-def.json -a "testScratch" -s
 
-sfdx force:source:push -u "testScratch"
+#sfdx force:source:push -u "testScratch"
 
-echo "Coverting source to metadata format"
-sfdx force:source:convert -d test_code -r force-app
+#echo "Coverting source to metadata format"
+#sfdx force:source:convert -d test_code -r force-app
 
-echo "Deploying code to org"
-sfdx force:mdapi:deploy --checkonly -u DevHub -d test_code/ -w -1 -l RunLocalTests
+#echo "Deploying code to org"
+#sfdx force:mdapi:deploy --checkonly -u DevHub -d test_code/ -w -1 -l RunLocalTests
 
 echo "Deploying source to org"
-sfdx force:source:deploy --sourcepath force-app --targetusername DevHub
+sfdx force:source:deploy --sourcepath force-app/main/default/classes --targetusername DevHub
 
 echo "Testing code in org"
 sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername DevHub
